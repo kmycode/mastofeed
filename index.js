f83fdd3a8df7f5f1c2cdbd638c02b65967043bd9
tt@kmycode.net <tt@kmycode.net>
2023-11-17 08:45:11 +0900

    Bump version to 5.9, 9.0

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ app.get('/apiv2/feed',cors(),logger,function(req,res){
 
 // 新しいものから順に
 const KMYBLUE_VERSIONS = [
+	{ major: 9, minor: 0, urgent: false, urgent_cross_version: false, },
 	{ major: 8, minor: 1, urgent: false, urgent_cross_version: false, },
 	{ major: 8, minor: 0, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 7, minor: 2, urgent: true, urgent_cross_version: false, },
@@ -124,6 +125,7 @@ const KMYBLUE_VERSIONS = [
 	{ major: 7, minor: 0, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 1, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 0, urgent: false, urgent_cross_version: false, },
+	{ major: 5, minor: 9, urgent: false, urgent_cross_version: false, },
 	{ major: 5, minor: 8, urgent: false, urgent_cross_version: false, },
 	{ major: 5, minor: 7, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 6, urgent: true, urgent_cross_version: false, },
