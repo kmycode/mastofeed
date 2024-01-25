a37ffa332d1e199fa35172159d77fd7f81338db0
tt@kmycode.net <tt@kmycode.net>
2024-01-25 18:06:42 +0900

    Mark as urgent

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.get('/apiv2/feed',cors(),logger,function(req,res){
 
 // 新しいものから順に
 const KMYBLUE_VERSIONS = [
-	{ major: 10, minor: 1, urgent: false, urgent_cross_version: false, },
+	{ major: 10, minor: 1, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 10, minor: 0, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 9, minor: 4, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 9, minor: 3, urgent: false, urgent_cross_version: false, },
@@ -131,7 +131,7 @@ const KMYBLUE_VERSIONS = [
 	{ major: 7, minor: 0, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 1, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 0, urgent: false, urgent_cross_version: false, },
-	{ major: 5, minor: 14, urgent: false, urgent_cross_version: false, },
+	{ major: 5, minor: 14, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 13, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 12, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 11, urgent: false, urgent_cross_version: false, },
