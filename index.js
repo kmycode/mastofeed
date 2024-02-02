5509d043d5d0df4aa0fbe34c4b7c2aa72458fc62
tt@kmycode.net <tt@kmycode.net>
2024-02-02 09:26:48 +0900

    Bump version to 5.15 LTS / 10.3

--- index.js
+++ index.js
@@ -117,6 +117,8 @@ app.get('/apiv2/feed',cors(),logger,function(req,res){
 
 // 新しいものから順に
 const KMYBLUE_VERSIONS = [
+	{ major: 10, minor: 3, urgent: true, urgent_cross_version: true, cross_by: 1, },
+	{ major: 10, minor: 2, urgent: false, urgent_cross_version: false, },
 	{ major: 10, minor: 1, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 10, minor: 0, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 9, minor: 4, urgent: true, urgent_cross_version: true, cross_by: 1, },
@@ -131,6 +133,7 @@ const KMYBLUE_VERSIONS = [
 	{ major: 7, minor: 0, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 1, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 0, urgent: false, urgent_cross_version: false, },
+	{ major: 5, minor: 15, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 14, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 13, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 12, urgent: true, urgent_cross_version: true, cross_by: 1, },
