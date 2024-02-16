745473f4316c51f45515e22593bd38f34f06cbf6
tt@kmycode.net <tt@kmycode.net>
2024-02-16 22:13:13 +0900

    Bump version to 10.5/5.17 LTS

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ app.get('/apiv2/feed',cors(),logger,function(req,res){
 
 // 新しいものから順に
 const KMYBLUE_VERSIONS = [
+	{ major: 10, minor: 5, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 10, minor: 4, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 10, minor: 3, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 10, minor: 2, urgent: false, urgent_cross_version: false, },
@@ -134,6 +135,7 @@ const KMYBLUE_VERSIONS = [
 	{ major: 7, minor: 0, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 1, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 6, minor: 0, urgent: false, urgent_cross_version: false, },
+	{ major: 5, minor: 17, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 16, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 15, urgent: true, urgent_cross_version: true, cross_by: 1, },
 	{ major: 5, minor: 14, urgent: true, urgent_cross_version: true, cross_by: 1, },
