f894a741a0eb982e25b6f03e230d19b0fcd74cbc
tt@kmycode.net <tt@kmycode.net>
2024-03-12 08:34:26 +0900

    Bump version to 11.3

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ app.get('/apiv2/feed',cors(),logger,function(req,res){
 
 // 新しいものから順に
 const KMYBLUE_VERSIONS = [
+	{ major: 11, minor: 3, urgent: false, urgent_cross_version: false, },
 	{ major: 11, minor: 2, urgent: false, urgent_cross_version: false, },
 	{ major: 11, minor: 1, urgent: true, urgent_cross_version: false, },
 	{ major: 11, minor: 0, urgent: false, urgent_cross_version: false, },
